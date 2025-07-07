4e15023d48be4754735be39592276cb896385aae
[Corinne Padilla] <[corinnepadilla@yahoo.com]>
2025-07-07 05:47:19 -0700

    modified folders

--- mybusinessportfolio/src/components/NavBar.js
+++ mybusinessportfolio/src/components/NavBar.js
@@ -1,7 +1,6 @@
 // src/components/NavBar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/NavBar.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
