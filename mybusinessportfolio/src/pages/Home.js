79c42397e66612bc98e42b3275b2a2ffa0d3aebb
[Corinne Padilla] <[corinnepadilla@yahoo.com]>
2025-07-12 16:17:38 -0700

    moved logo images folder into public folder, updated imports and logomodal.js

--- mybusinessportfolio/src/pages/Home.js
+++ mybusinessportfolio/src/pages/Home.js
@@ -1,40 +1,42 @@
 // src/pages/Home.js
 import React, { useState } from "react";
-import logoSmall from "../images/logo_small.jpeg";
-import logoMedium from "../images/logo_medium.jpeg";
-import logoLarge from "../images/logo_large.jpeg";
 import LogoModal from "../components/LogoModal";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function Home() {
   const [showModal, setShowModal] = useState(false);
 
+  // Direct URLs for public/ images
+  const logoSmall = "/images/logo_small.jpeg";
+  const logoMedium = "/images/logo_medium.jpeg";
+  const logoLarge = "/images/logo_large.jpeg";
+
   return (
     <div className="container-fluid">
-        <h1 className="page-header">Welcome to Mom & Pop Shop Web Design</h1>
-        <div className="text-center">
-          <img
-            src={logoMedium}
-            srcSet={`${logoSmall} 150w, ${logoMedium} 300w, ${logoLarge} 600w`}
-            sizes="(max-width: 600px) 150px, (max-width: 900px) 300px, 600px"
-            alt="Mom & Pop Shop Logo"
-            style={{ cursor: "pointer", maxWidth: "100%" }}
-            onClick={() => setShowModal(true)}
-          />
+      <h1 className="page-header">Welcome to Mom & Pop Shop Web Design</h1>
+      <div className="text-center">
+        <img
+          src={logoMedium}
+          srcSet={`${logoSmall} 150w, ${logoMedium} 300w, ${logoLarge} 600w`}
+          sizes="(max-width: 600px) 150px, (max-width: 900px) 300px, 600px"
+          alt="Mom & Pop Shop Logo"
+          style={{ cursor: "pointer", maxWidth: "100%" }}
+          onClick={() => setShowModal(true)}
+        />
 
-          <p className="subheader">
-            Helping local businesses build simple, beautiful websites that grow
-            their customer base.
-          </p>
-          <p className="subheader">
-            Whether you need a brand new website or a refresh, I specialize in
-            creating easy-to-manage, responsive sites using HTML, CSS,
-            JavaScript, and React. I can use backend code for form submission success!
-          </p>
+        <p className="subheader">
+          Helping local businesses build simple, beautiful websites that grow
+          their customer base.
+        </p>
+        <p className="subheader">
+          Whether you need a brand new website or a refresh, I specialize in
+          creating easy-to-manage, responsive sites using HTML, CSS, JavaScript,
+          and React. I can use backend code for form submission success!
+        </p>
 
-          <a href="/services" className="custom-button">
-            View Our Services
-          </a>
+        <a href="/services" className="custom-button">
+          View Our Services
+        </a>
       </div>
 
       {/* Modal for Download/Print */}
