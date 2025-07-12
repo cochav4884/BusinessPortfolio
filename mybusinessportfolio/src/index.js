a56ae1423cc946e49f97b2e68827d5256948938b
[Corinne Padilla] <[corinnepadilla@yahoo.com]>
2025-07-12 10:13:22 -0700

    changed BrowserRouter to HashRouther in index.js to fix the blank screen in my published BuisnessPortfolio Website

--- mybusinessportfolio/src/index.js
+++ mybusinessportfolio/src/index.js
@@ -4,8 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+import { HashRouter } from "react-router-dom";
+
+<HashRouter>
+  <App />
+</HashRouter>
