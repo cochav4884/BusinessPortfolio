9c6ce6072ed2472d0e05801887478022e49a6d14
[Corinne Padilla] <[corinnepadilla@yahoo.com]>
2025-07-19 19:20:45 -0700

    updated LandingPage.js and LogoModal.js pages to use favicon.co logo image

--- mybusinessportfolio/src/components/LogoModal.js
+++ mybusinessportfolio/src/components/LogoModal.js
@@ -2,35 +2,39 @@
 import React from "react";
 import { Modal, Button } from "react-bootstrap";
 
-function LogoModal({ show, handleClose, logoSmall, logoMedium, logoLarge }) {
+function LogoModal({ show, handleClose }) {
+  const logoPath = "/favicon.ico";
+
   return (
     <Modal show={show} onHide={handleClose} centered>
       <Modal.Header closeButton>
         <Modal.Title>Download or Print Logo</Modal.Title>
       </Modal.Header>
       <Modal.Body className="text-center">
-        <p>Select a size to download:</p>
-        <div className="d-flex justify-content-center gap-2 mb-3 flex-wrap">
-          <a href={logoSmall} download="MomPopLogo_Small.jpeg">
-            Small (150px)
+        <img
+          src={logoPath}
+          alt="Logo Preview"
+          style={{ width: "100px", height: "100px", marginBottom: "1rem" }}
+        />
+        <div className="d-flex justify-content-center gap-3 mb-3">
+          <a
+            href={logoPath}
+            download="MomPopLogo.ico"
+            className="btn btn-primary"
+          >
+            Download
           </a>
-          <a href={logoMedium} download="MomPopLogo_Medium.jpeg">
-            Medium (300px)
-          </a>
-          <a href={logoLarge} download="MomPopLogo_Large.jpeg">
-            Large (600px)
-          </a>
+          <Button
+            variant="outline-secondary"
+            onClick={() => {
+              const printWindow = window.open(logoPath, "_blank");
+              printWindow?.focus();
+              printWindow?.print();
+            }}
+          >
+            Print Logo
+          </Button>
         </div>
-        <Button
-          variant="outline-secondary"
-          onClick={() => {
-            const printWindow = window.open(logoLarge, "_blank");
-            printWindow?.focus();
-            printWindow?.print();
-          }}
-        >
-          Print Logo
-        </Button>
       </Modal.Body>
     </Modal>
   );
--- mybusinessportfolio/src/pages/LandingPage.js
+++ mybusinessportfolio/src/pages/LandingPage.js
@@ -1,33 +1,22 @@
 // src/pages/Home.js
 import React, { useState } from "react";
-import { Link } from "react-router-dom"; // make sure this is at the top
+import { Link } from "react-router-dom";
 import LogoModal from "../components/LogoModal";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function LandingPage() {
   const [showModal, setShowModal] = useState(false);
 
-  console.log("CloudFront URL:", process.env.REACT_APP_CLOUDFRONT_URL);
-
-  // Direct URLs for public/ images
-  const logoSmall = "/images/logo_small.jpeg";
-  const logoMedium = "/images/logo_medium.jpeg";
-  const logoLarge = "/images/logo_large.jpeg";
+  const faviconPath = "/favicon.ico"; // reliable public path
 
   return (
     <div className="container-fluid">
       <h1 className="page-header">Welcome to Mom & Pop Shop Web Design</h1>
       <div className="text-center">
         <img
-          src={`${process.env.REACT_APP_CLOUDFRONT_URL}/images/logo_medium.jpeg`}
-          srcSet={`
-    ${process.env.REACT_APP_CLOUDFRONT_URL}/images/logo_small.jpeg 150w,
-    ${process.env.REACT_APP_CLOUDFRONT_URL}/images/logo_medium.jpeg 300w,
-    ${process.env.REACT_APP_CLOUDFRONT_URL}/images/logo_large.jpeg 600w
-  `}
-          sizes="(max-width: 600px) 150px, (max-width: 900px) 300px, 600px"
+          src={faviconPath}
           alt="Mom & Pop Shop Logo"
-          style={{ cursor: "pointer", maxWidth: "100%" }}
+          style={{ cursor: "pointer", maxWidth: "100px", marginBottom: "1rem" }}
           onClick={() => setShowModal(true)}
         />
 
@@ -51,9 +40,6 @@ function LandingPage() {
       <LogoModal
         show={showModal}
         handleClose={() => setShowModal(false)}
-        logoSmall={logoSmall}
-        logoMedium={logoMedium}
-        logoLarge={logoLarge}
       />
     </div>
   );
