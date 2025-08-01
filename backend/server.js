95390008fcf15a9cae2f231de48b8ca0c6aa81b5
[Corinne Padilla] <[corinnepadilla@yahoo.com]>
2025-08-01 03:57:49 -0700

    added tests insdie the const transpoter nodemailer in server.js

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,8 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  logger: true,   // Enable logging to console
+  debug: true,    // Include SMTP debug output
 });
 
 // Verify transporter on startup
